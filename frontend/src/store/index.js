9fd91320d76786adee0129f74e6d98d905134adf
Thibault Nocchi <thibaultnocchi@gmail.com>
2019-11-26 16:35:10 +0100

    Frontend: retrieving current persons number.

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -5,10 +5,17 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    current: 0,
   },
   mutations: {
+    set_current(state, val) {
+      state.current = val;
+    },
   },
   actions: {
+    retrieve_current(context) {
+      fetch('http://localhost:8081/client').then(r => r.json()).then(j => context.commit('set_current', j.in)).catch(e => console.log(e));
+    },
   },
   modules: {
   },
